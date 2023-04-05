41635ff23afd192f4d346dbea1380d721b2dcafd
ChristopherVuvu <vuvumumo93@gmail.com>
2023-04-05 14:07:42 +0300

    added

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+    const myTextElement = document.getElementById('text');
+    myTextElement.innerText = 'This is really cool!';
+});
+
